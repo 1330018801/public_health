9d9b3bb8b18474ac521ff76aad6eab03c1225206
lijun <lijun@seimun.com>
2015-10-09 22:34:13 +0800

    debug: child 0-1, button enable and disable

--- static/js/child_health_0_1.js
+++ static/js/child_health_0_1.js
@@ -3,7 +3,6 @@ $(function () {
     var toolbar = $('#toolbar');
     var form = $('#form');
     var panel = $('#table');
-    var resident_id = $('#resident_id').val();
     var item_alias = undefined;
 
     var btn_add_1 = toolbar.find('#add_1').linkbutton({ iconCls: 'icon-add', plain: true });
@@ -18,183 +17,261 @@ $(function () {
 
 
     btn_add_1.bind('click', function () {
-        panel.panel({
-            href: '/child/health_0_1_form/', method: 'POST',
-            queryParams: { item_alias: 'aftercare_1_month' }
-        });
-        item_alias =  'aftercare_1_month';
-        btn_save.linkbutton('enable');
-        btn_undo.linkbutton('enable');
+        if ($(this).linkbutton('options').disabled == false) {
+            panel.panel({
+                href: '/child/health_0_1_form/', method: 'POST',
+                queryParams: { item_alias: 'aftercare_1_month' },
+                onLoad: function () {}
+            });
+            item_alias = 'aftercare_1_month';
+            btn_add_1.linkbutton('disable');
+            btn_add_2.linkbutton('disable');
+            btn_add_3.linkbutton('disable');
+            btn_add_4.linkbutton('disable');
+            btn_save.linkbutton('enable');
+            btn_undo.linkbutton('enable');
+            btn_print.linkbutton('disable');
+        }
     });
 
     btn_add_2.bind('click', function () {
-        panel.panel({
-            href: '/child/health_0_1_form/', method: 'POST',
-            queryParams: { item_alias: 'aftercare_3_month' }
-        });
-        item_alias =  'aftercare_3_month';
-        btn_save.linkbutton('enable');
-        btn_undo.linkbutton('enable');
+        if ($(this).linkbutton('options').disabled == false) {
+            panel.panel({
+                href: '/child/health_0_1_form/', method: 'POST',
+                queryParams: { item_alias: 'aftercare_3_month' },
+                onLoad: function () {}
+            });
+            item_alias = 'aftercare_3_month';
+            btn_add_1.linkbutton('disable');
+            btn_add_2.linkbutton('disable');
+            btn_add_3.linkbutton('disable');
+            btn_add_4.linkbutton('disable');
+            btn_save.linkbutton('enable');
+            btn_undo.linkbutton('enable');
+            btn_print.linkbutton('disable');
+        }
     });
 
 
     btn_add_3.bind('click', function () {
-        panel.panel({
-            href: '/child/health_0_1_form/', method: 'POST',
-            queryParams: { item_alias: 'aftercare_6_month' }
-        });
-        item_alias =  'aftercare_6_month';
-        btn_save.linkbutton('enable');
-        btn_undo.linkbutton('enable');
+        if ($(this).linkbutton('options').disabled == false) {
+            panel.panel({
+                href: '/child/health_0_1_form/', method: 'POST',
+                queryParams: { item_alias: 'aftercare_6_month' },
+                onLoad: function () {}
+            });
+            item_alias = 'aftercare_6_month';
+            btn_add_1.linkbutton('disable');
+            btn_add_2.linkbutton('disable');
+            btn_add_3.linkbutton('disable');
+            btn_add_4.linkbutton('disable');
+            btn_save.linkbutton('enable');
+            btn_undo.linkbutton('enable');
+            btn_print.linkbutton('disable');
+        }
     });
 
     btn_add_4.bind('click', function () {
-        panel.panel({
-            href: '/child/health_0_1_form/', method: 'POST',
-            queryParams: { item_alias: 'aftercare_8_month' }
-        });
-        item_alias =  'aftercare_8_month';
-        btn_save.linkbutton('enable');
-        btn_undo.linkbutton('enable');
+        if ($(this).linkbutton('options').disabled == false) {
+            panel.panel({
+                href: '/child/health_0_1_form/', method: 'POST',
+                queryParams: { item_alias: 'aftercare_8_month' },
+                onLoad: function () {}
+            });
+            item_alias = 'aftercare_8_month';
+            btn_add_1.linkbutton('disable');
+            btn_add_2.linkbutton('disable');
+            btn_add_3.linkbutton('disable');
+            btn_add_4.linkbutton('disable');
+            btn_save.linkbutton('enable');
+            btn_undo.linkbutton('enable');
+            btn_print.linkbutton('disable');
+        }
     });
 
     btn_save.bind('click', function () {
-        form.form('submit', {
-            url: '/child/health_0_1_submit/',
-            onSubmit: function (param) {
-                if(!form.find('input[name=weight_grade]').is(":checked")){
-                    $.messager.alert('提示', '请选择体重等级', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=height_grade]').is(":checked")){
-                    $.messager.alert('提示', '请选择身高等级', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=complexion]').is(":checked")){
-                    $.messager.alert('提示', '请选择面色情况', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=skin]').is(":checked")){
-                    $.messager.alert('提示', '请选择皮肤是否异常', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=bregma]').is(":checked")){
-                    $.messager.alert('提示', '请选择前囟是否闭合', 'info');
-                    return false;
-                }
-                if(item_alias != 'aftercare_8_month'){
-                    if(!form.find('input[name=neck_enclosed_mass]').is(":checked")){
-                        $.messager.alert('提示', '请选择是否有颈部包块', 'info');
+        if ($(this).linkbutton('options').disabled == false) {
+            form.form('submit', {
+                url: '/child/health_0_1_submit/',
+                onSubmit: function (param) {
+                    if (!form.find('input[name=weight_grade]').is(":checked")) {
+                        $.messager.alert('提示', '请选择体重等级', 'info');
                         return false;
                     }
-                }
-                if(!form.find('input[name=eye_appearance]').is(":checked")){
-                    $.messager.alert('提示', '请选择眼外观是否异常', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=ear_appearance]').is(":checked")){
-                    $.messager.alert('提示', '请选择耳外观是否异常', 'info');
-                    return false;
-                }
-                if(item_alias == 'aftercare_6_month'){
-                    if(!form.find('input[name=hearing]').is(":checked")){
-                        $.messager.alert('提示', '请选择听力是否通过', 'info');
+                    if (!form.find('input[name=height_grade]').is(":checked")) {
+                        $.messager.alert('提示', '请选择身高等级', 'info');
                         return false;
                     }
-                }
-                if(item_alias == 'aftercare_1_month' | item_alias == 'aftercare_3_month'){
-                    if(!form.find('input[name=oral_cavity]').is(":checked")){
-                        $.messager.alert('提示', '请选择口腔是否异常', 'info');
+                    if (!form.find('input[name=complexion]').is(":checked")) {
+                        $.messager.alert('提示', '请选择面色情况', 'info');
                         return false;
                     }
-                }
-                if(!form.find('input[name=heart_lung]').is(":checked")){
-                    $.messager.alert('提示', '请选择心肺是否异常', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=abdomen]').is(":checked")){
-                    $.messager.alert('提示', '请选择腹部是否异常', 'info');
-                    return false;
-                }
-                if(item_alias == 'aftercare_1_month' | item_alias == 'aftercare_3_month'){
-                    if(!form.find('input[name=navel]').is(":checked")){
-                        $.messager.alert('提示', '请选择脐部是否异常', 'info');
+                    if (!form.find('input[name=skin]').is(":checked")) {
+                        $.messager.alert('提示', '请选择皮肤是否异常', 'info');
                         return false;
                     }
-                }
-                if(!form.find('input[name=all_fours]').is(":checked")){
-                    $.messager.alert('提示', '请选择四肢是否异常', 'info');
-                    return false;
-                }
-                if(item_alias != 'aftercare_1_month'){
-                    if(!form.find('input[name=rickets_symptom]').is(":checked")){
-                        $.messager.alert('提示', '请选择可疑佝偻病症状', 'info');
+                    if (!form.find('input[name=bregma]').is(":checked")) {
+                        $.messager.alert('提示', '请选择前囟是否闭合', 'info');
                         return false;
                     }
-                }
-                if(item_alias == 'aftercare_1_month' | item_alias == 'aftercare_3_month'){
-                    if(!form.find('input[name=rickets_sign]').is(":checked")){
-                        $.messager.alert('提示', '请选择可疑佝偻病体征', 'info');
+                    if (item_alias != 'aftercare_8_month') {
+                        if (!form.find('input[name=neck_enclosed_mass]').is(":checked")) {
+                            $.messager.alert('提示', '请选择是否有颈部包块', 'info');
+                            return false;
+                        }
+                    }
+                    if (!form.find('input[name=eye_appearance]').is(":checked")) {
+                        $.messager.alert('提示', '请选择眼外观是否异常', 'info');
                         return false;
                     }
-                }
-                if(!form.find('input[name=anus_externalia]').is(":checked")){
-                    $.messager.alert('提示', '请选择肛门/外生殖是否异常', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=growth_evaluate]').is(":checked")){
-                    $.messager.alert('提示', '请选择发育评估情况', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=two_visit_disease]').is(":checked")){
-                    $.messager.alert('提示', '请选择两次随访间患病情况', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=transfer_treatment_suggestion]').is(":checked")){
-                    $.messager.alert('提示', '请选择转诊建议', 'info');
-                    return false;
-                }
-                if(!form.find('input[name=guide]').is(":checked")){
-                    $.messager.alert('提示', '请选择指导方式', 'info');
-                    return false;
-                }
+                    if (!form.find('input[name=ear_appearance]').is(":checked")) {
+                        $.messager.alert('提示', '请选择耳外观是否异常', 'info');
+                        return false;
+                    }
+                    if (item_alias == 'aftercare_6_month') {
+                        if (!form.find('input[name=hearing]').is(":checked")) {
+                            $.messager.alert('提示', '请选择听力是否通过', 'info');
+                            return false;
+                        }
+                    }
+                    if (item_alias == 'aftercare_1_month' | item_alias == 'aftercare_3_month') {
+                        if (!form.find('input[name=oral_cavity]').is(":checked")) {
+                            $.messager.alert('提示', '请选择口腔是否异常', 'info');
+                            return false;
+                        }
+                    }
+                    if (!form.find('input[name=heart_lung]').is(":checked")) {
+                        $.messager.alert('提示', '请选择心肺是否异常', 'info');
+                        return false;
+                    }
+                    if (!form.find('input[name=abdomen]').is(":checked")) {
+                        $.messager.alert('提示', '请选择腹部是否异常', 'info');
+                        return false;
+                    }
+                    if (item_alias == 'aftercare_1_month' | item_alias == 'aftercare_3_month') {
+                        if (!form.find('input[name=navel]').is(":checked")) {
+                            $.messager.alert('提示', '请选择脐部是否异常', 'info');
+                            return false;
+                        }
+                    }
+                    if (!form.find('input[name=all_fours]').is(":checked")) {
+                        $.messager.alert('提示', '请选择四肢是否异常', 'info');
+                        return false;
+                    }
+                    if (item_alias != 'aftercare_1_month') {
+                        if (!form.find('input[name=rickets_symptom]').is(":checked")) {
+                            $.messager.alert('提示', '请选择可疑佝偻病症状', 'info');
+                            return false;
+                        }
+                    }
+                    if (item_alias == 'aftercare_1_month' | item_alias == 'aftercare_3_month') {
+                        if (!form.find('input[name=rickets_sign]').is(":checked")) {
+                            $.messager.alert('提示', '请选择可疑佝偻病体征', 'info');
+                            return false;
+                        }
+                    }
+                    if (!form.find('input[name=anus_externalia]').is(":checked")) {
+                        $.messager.alert('提示', '请选择肛门/外生殖是否异常', 'info');
+                        return false;
+                    }
+                    if (!form.find('input[name=growth_evaluate]').is(":checked")) {
+                        $.messager.alert('提示', '请选择发育评估情况', 'info');
+                        return false;
+                    }
+                    if (!form.find('input[name=two_visit_disease]').is(":checked")) {
+                        $.messager.alert('提示', '请选择两次随访间患病情况', 'info');
+                        return false;
+                    }
+                    if (!form.find('input[name=transfer_treatment_suggestion]').is(":checked")) {
+                        $.messager.alert('提示', '请选择转诊建议', 'info');
+                        return false;
+                    }
+                    if (!form.find('input[name=guide]').is(":checked")) {
+                        $.messager.alert('提示', '请选择指导方式', 'info');
+                        return false;
+                    }
 
-                if(form.form('validate')){
-                    param.csrfmiddlewaretoken = $.cookie('csrftoken');
-                    param.item_alias = item_alias;
-                    return true;
-                }
-                else{
-                    return false;
-                }
+                    if (form.form('validate')) {
+                        param.csrfmiddlewaretoken = $.cookie('csrftoken');
+                        param.item_alias = item_alias;
+                        return true;
+                    }
+                    else {
+                        return false;
+                    }
 
-            },
-            success: function (data) {
-                var data_obj = eval('(' + data + ')');
-                if (data_obj.success) {
-                    $.messager.show({title: '提示', msg: '随访记录保存成功', timeout: 1000});
-                } else {
-                    $.messager.alert('提示', '随访记录保存失败', 'warning');
+                },
+                success: function (data) {
+                    var data_obj = eval('(' + data + ')');
+                    if (data_obj.success) {
+                        $.messager.show({title: '提示', msg: '随访记录保存成功', timeout: 1000});
+                    } else {
+                        $.messager.alert('提示', '随访记录保存失败', 'warning');
+                    }
+                    panel.panel({ href: '/child/health_0_1_review/' });
+                    btn_save.linkbutton('disable');
+                    btn_undo.linkbutton('disable');
+                    btn_print.linkbutton('enable');
                 }
-                panel.panel({ href: '/child/health_0_1_review/' });
-                btn_save.linkbutton('disable');
-                btn_undo.linkbutton('disable');
-            }
-        });
+            });
+        }
     });
 
     btn_undo.bind('click', function () {
-        panel.panel({ href: '/child/health_0_1_review/' });
-        btn_add_1.linkbutton('enable');
-        btn_add_2.linkbutton('enable');
-        btn_add_3.linkbutton('enable');
-        btn_add_4.linkbutton('enable');
+        if ($(this).linkbutton('options').disabled == false) {
+            panel.panel({
+                href: '/child/health_0_1_review/',
+                onLoad: function () {
+                    if ($.trim(panel.find('.1_month').html())) {
+                        btn_add_1.linkbutton('disable');
+                    } else {
+                        btn_add_1.linkbutton('enable');
+                    }
+                    if ($.trim(panel.find('.3_month').html())) {
+                        btn_add_2.linkbutton('disable');
+                    } else {
+                        btn_add_2.linkbutton('enable');
+                    }
+                    if ($.trim(panel.find('.6_month').html())) {
+                        btn_add_3.linkbutton('disable');
+                    } else {
+                        btn_add_3.linkbutton('enable');
+                    }
+                    if ($.trim(panel.find('.8_month').html())) {
+                        btn_add_4.linkbutton('disable');
+                    } else {
+                        btn_add_4.linkbutton('enable');
+                    }
+                }
+            });
+            btn_save.linkbutton('disable');
+            btn_undo.linkbutton('disable');
+            btn_print.linkbutton('enable');
+        }
     });
 
     btn_print.bind('click', function () {
-        panel.find('.print_area').printThis();
+        if ($(this).linkbutton('options').disabled == false) {
+            panel.find('.print_area').printThis();
+        }
     });
 
-    panel.panel({ href: '/child/health_0_1_review/' });
+    panel.panel({
+        href: '/child/health_0_1_review/',
+        onLoad: function () {
+            if ($.trim(panel.find('.1_month').html())) {
+                btn_add_1.linkbutton('disable');
+            }
+            if ($.trim(panel.find('.3_month').html())) {
+                btn_add_2.linkbutton('disable');
+            }
+            if ($.trim(panel.find('.6_month').html())) {
+                btn_add_3.linkbutton('disable');
+            }
+            if ($.trim(panel.find('.8_month').html())) {
+                btn_add_4.linkbutton('disable');
+            }
+        }
+    });
 
 });
