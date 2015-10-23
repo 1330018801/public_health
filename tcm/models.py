6e333398a0b7ace169bf72fdaedf7b3e24d8e28e
zhiqing <zhiqing@seimun.com>
2015-10-23 19:36:51 +0800

    “老年人中医药健康管理服务记录表”已经完成了表格、model和fixtures，正在写views和js。

--- tcm/models.py
+++ tcm/models.py
@@ -54,3 +54,127 @@ class Aftercare(models.Model):
     class Meta:
         db_table = 'tcm_child_aftercare'
 
+
+CHOICES = ((1, '1'), (2, '2'), (3, '3'), (4, '4'), (5, '5'))
+
+
+class TcmOldIdentifyChoicesAbstract(models.Model):
+    choice = models.CharField(max_length=50, primary_key=True)
+    order = models.PositiveSmallIntegerField()
+
+    def __unicode__(self):
+        return self.choice
+
+    class Meta:
+        abstract = True
+        ordering = ('order',)
+
+
+class TcmOldIdentifyChoicesQixu(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesYangxu(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesYinxu(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesTanshi(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesShire(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesXueyu(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesQiyu(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesTebing(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class TcmOldIdentifyChoicesPinghe(TcmOldIdentifyChoicesAbstract):
+    pass
+
+
+class OldIdentify(models.Model):
+    q1 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q2 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q3 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q4 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q5 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q6 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q7 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q8 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q9 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q10 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q11 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q12 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q13 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q14 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q15 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q16 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q17 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q18 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q19 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q20 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q21 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q22 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q23 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q24 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q25 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q26 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q27 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q28 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q29 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q30 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q31 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q32 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    q33 = models.PositiveSmallIntegerField(verbose_name='', choices=CHOICES, blank=True, null=True)
+    constitution_identify_points_qixu = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_yangxu = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_yinxu = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_tanshi = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_shire = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_xueyu = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_qiyu = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_tebing = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_points_pinghe = models.PositiveSmallIntegerField(verbose_name='', blank=True, null=True)
+    constitution_identify_yes_trend_qixu = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_yangxu = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_yinxu = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_tanshi = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_shire = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_xueyu = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_qiyu = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_tebing = models.CharField(max_length=20, blank=True, null=True)
+    constitution_identify_yes_trend_pinghe = models.CharField(max_length=20, blank=True, null=True)
+    health_care_guide_qixu = models.ManyToManyField(TcmOldIdentifyChoicesQixu, verbose_name='', blank=True, null=True)
+    health_care_guide_yangxu = models.ManyToManyField(TcmOldIdentifyChoicesYangxu, verbose_name='', blank=True, null=True)
+    health_care_guide_yinxu = models.ManyToManyField(TcmOldIdentifyChoicesYinxu, verbose_name='', blank=True, null=True)
+    health_care_guide_tanshi = models.ManyToManyField(TcmOldIdentifyChoicesTanshi, verbose_name='', blank=True, null=True)
+    health_care_guide_shire = models.ManyToManyField(TcmOldIdentifyChoicesShire, verbose_name='', blank=True, null=True)
+    health_care_guide_xueyu = models.ManyToManyField(TcmOldIdentifyChoicesXueyu, verbose_name='', blank=True, null=True)
+    health_care_guide_qiyu = models.ManyToManyField(TcmOldIdentifyChoicesQiyu, verbose_name='', blank=True, null=True)
+    health_care_guide_tebing = models.ManyToManyField(TcmOldIdentifyChoicesTebing, verbose_name='', blank=True, null=True)
+    health_care_guide_pinghe = models.ManyToManyField(TcmOldIdentifyChoicesPinghe, verbose_name='', blank=True, null=True)
+    health_care_guide_extra_qixu = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_yangxu = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_yinxu = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_tanshi = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_shire = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_xueyu = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_qiyu = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_tebing = models.CharField(max_length=200, blank=True, null=True)
+    health_care_guide_extra_pinghe = models.CharField(max_length=200, blank=True, null=True)
+    fill_table_date = models.DateField(blank=True, null=True)
+    doctor_signature = models.CharField(max_length=50, blank=True, null=True)
